e69ef7d1c06c7d5350fb647c99c08321d6341c2c
Andy Tamayo <andresjt@icloud.com>
2022-03-23 00:16:28 -0600

    added favicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,11 @@ const Home = (Data: Data) => {
         <title>Emprendimiento: Comprendio de Errores de Inicio</title>
         <meta name="description" content="La cultura en la que crecemos muchos latinoamericanos no favorece una mentalidad adecuada frente al emprendimiento. Este escrito pretende ayudar a corregir varios de los errores más comunes que surgen de este problema a la hora de comenzar a emprender." />
         <meta name="keywords" content="Emprendimiento, Emprendedurismo, Latinoamérica, Errores, Comenzar, Comienzo" />
+        <link rel='icon' type='image/png' href='/favicon/favicon.png'></link>
+        <meta property="og:title" content="Emprendimiento: Compendio de Errores de Inicio" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://libro-ecei.web.app" />
+        <meta property="og:image" content="/favicon/ogimage.png" />
       </Head>
       {Data.error ? <p>Error getting content</p> : <ArticleLayout {...Data} />}
     </>
